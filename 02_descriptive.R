6ddcd0576bd8ac5300fcecade72ae8a688a3abce
weidc1998 <weidc1998@163.com>
2026-01-14 12:25:05 +0800

    Create 02_descriptive.R

---
+++ 02_descriptive.R
@@ -0,0 +1,14 @@
+############################################################
+# Script: 02_descriptive.R
+# Purpose: Summary statistics and table generation
+############################################################
+
+library(tableone)
+
+# Load processed data
+data <- read.csv("data/processed/data_imputed_scaled.csv")
+
+# ---- Create table for baseline characteristics ----
+Tab1.stas <- CreateTableOne(vars = colnames(data), strata = "group", data = data, factorVars = c("group", "sex", "diab", "hyper"), test = TRUE, smd = TRUE, addOverall = TRUE)
+Tab1 <- print(Tab1.stas, catDigits = 1, contDigits = 2, pDigits = 3, test = TRUE, smd = TRUE, noSpaces = TRUE)
+write.csv(Tab1, file = "results/Table1.csv")
